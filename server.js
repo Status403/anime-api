fa6af345ab29cc05e598e1752a20dae6175fa4a3
Fumano <marcdecker2004@gmail.com>
2023-04-20 22:15:03 +0200

    refactoring

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ app.get('/episode', async(req, res) => {
     try {
         const s = req.body.season;
         const e = req.body.episode;
+        if (!e || !s) {
+            throw Error('Need Season and Episode params!')
+        }
         const episode = await Episode.findOne({'season': s, 'episode': e});
-        res.status(200).json(episode);
+        res.status(200).send(episode);
     } catch (error) {
         console.log(error)
         res.status(500).json({message: error.message})
