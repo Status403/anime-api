b0f295096d4246b8b16b3c6dce21128b72f5c176
Fumano <marcdecker2004@gmail.com>
2023-04-20 22:53:19 +0200

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
 const mongoose = require('mongoose')
 const Episode = require('./models/episode');
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
